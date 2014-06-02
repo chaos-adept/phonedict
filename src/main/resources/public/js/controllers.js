b5367fe97878ccf11d9164e27f69d57b60269493
chaos-adept <denis.rykovanov@gmail.com>
2014-06-02 10:18:44 +0400

    [ref] cosmetics

--- src/main/resources/public/js/controllers.js
+++ src/main/resources/public/js/controllers.js
@@ -5,35 +5,42 @@
 angular.module('phoneDictApp.controllers', [])
     .controller('DictController', ['$scope', '$http', '$modal', '$q', function ($scope, $http, $modal, $q) {
 
+        var RELOAD_FIRST_PAGE_TIME = 600;
+        var CONFLICT_STATUS = 409;
+        var PHONE_NUMBER_PATTERN =
+            /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/;
+
         $scope.pageNum = 0;
         $scope.pageSize = 50;
 
         $scope.items = [];
 
-        var phonePattern = /^(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\/]?){0,})(?:[\-\.\ \\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\/]?(\d+))?$/;
-
         $scope.editRequest = function (item) {
+
+
             var modalScope = $scope.$new(true);
-
             var originValues = { name: item.name, phoneNumber: item.phoneNumber };
-
-            modalScope.phonePattern = phonePattern;
-
+            modalScope.item = angular.copy(originValues);
+            modalScope.phonePattern = PHONE_NUMBER_PATTERN;
             modalScope.isUnchanged = function (currentValues) {
                 return angular.equals(currentValues, originValues);
             };
             modalScope.requestClosing = function (editedFields, closeHandler) {
                 $scope.save(item, editedFields).then(function () {
                     closeHandler();
-                }, function () {
                 });
             };
 
-            modalScope.item = angular.copy(originValues);
-
             $modal.open({
                 templateUrl: 'partial/contact-modal-dialog.html',
-                controller: ModalInstanceCtrl,
+                controller: function ($scope, $modalInstance) {
+                    $scope.ok = function () {
+                        $scope.requestClosing($scope.item, $modalInstance.close);
+                    };
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                },
                 scope: modalScope
             });
         };
@@ -42,21 +49,20 @@ angular.module('phoneDictApp.controllers', [])
             $http.delete(item._links.self.href).success(function () {
                 $scope.items.splice($scope.items.indexOf(item), 1);
             })
-                .error($scope.showGenericPersistContactError);
+            .error($scope.showGenericPersistContactError);
         };
 
         $scope.save = function (item, changedFields) {
-
             var deferred = $q.defer();
             if (item._links) {
                 $http({method: 'PATCH', url: item._links.self.href, data: changedFields}).success(function () {
                     $.extend(item, changedFields);
                     deferred.resolve();
                 })
-                    .error(function (data, status, headers) {
-                        deferred.reject();
-                        $scope.showGenericPersistContactError.apply($scope, arguments);
-                    });
+                .error(function (data, status, headers) {
+                    deferred.reject();
+                    $scope.showGenericPersistContactError.apply($scope, arguments);
+                });
             } else {
                 $http.post('/contacts', changedFields).success(function (data, status, headers) {
                     var selfLink = headers("location");
@@ -74,7 +80,6 @@ angular.module('phoneDictApp.controllers', [])
         };
 
         $scope.reLoadFirstPage = function () {
-
             $scope.isLoadingPage = true;
 
             var successHandler = function (data) {
@@ -94,18 +99,14 @@ angular.module('phoneDictApp.controllers', [])
         };
 
         $scope.nextPage = function () {
-
-
             if (!$scope.nextPageHref) {
                 return;
             }
-
             if ($scope.isLoadingPage) {
                 return;
             }
 
             $scope.isLoadingPage = true;
-
             $http.get($scope.nextPageHref).success(function (data) {
                 $scope.totalPages = data.page.totalPages;
                 $scope.nextPageHref = data._links.next ? data._links.next.href : undefined;
@@ -116,19 +117,8 @@ angular.module('phoneDictApp.controllers', [])
             });
         };
 
-
-        var ModalInstanceCtrl = function ($scope, $modalInstance) {
-
-            $scope.ok = function () {
-                $scope.requestClosing($scope.item, $modalInstance.close);
-            };
-
-            $scope.cancel = function () {
-                $modalInstance.dismiss('cancel');
-            };
-        };
-        $scope.showGenericPersistContactError = function (data, status, headers) {
-            if (status == 409) {
+        $scope.showGenericPersistContactError = function (data, status) {
+            if (status == CONFLICT_STATUS) {
                 alert('There is another user with same name and phone number. Please re-check your values.');
             } else {
                 var rawMsgPart = "Error. HTTP Status:'" + status + "', error data: " + JSON.stringify(data);
@@ -137,8 +127,7 @@ angular.module('phoneDictApp.controllers', [])
 
         };
 
-        $scope.debounceReLoadFirstPage = _.debounce($scope.reLoadFirstPage, 600);
-
+        $scope.debounceReLoadFirstPage = _.debounce($scope.reLoadFirstPage, RELOAD_FIRST_PAGE_TIME);
         $scope.debounceReLoadFirstPage();
 
     }]);
